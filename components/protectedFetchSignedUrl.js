4949eda005b24153d440a2a28f3c0da82b1bb28a
Moscalu Stefan <94412232+Sm18990@users.noreply.github.com>
2024-06-23 23:01:36 +0300

    91: Add jwt authentication for urlSigningEntity and create export possibility.

--- components/protectedFetchSignedUrl.js
+++ components/protectedFetchSignedUrl.js
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2024. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
 const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbnRpdHlJZCI6IjAyMjY0NjU3LTUwOWUtNGNhMS04ZTA0LTg5MmY3ZWI3MDRiMCIsImlhdCI6MTcxOTE3MDM3MywiZXhwIjoxNzE5MTczOTczfQ.4n4FqfW4puPvprbEO-ZZg6gJ7tqFvqoQ1t4yK3bubCU';
 async function fetchSignedURL(urlToSign) {
   try {
