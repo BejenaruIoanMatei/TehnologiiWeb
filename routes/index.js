f5c15a8e69b0613e51905987d3f4b67ac3fc5375
Moscalu Stefan <94412232+Sm18990@users.noreply.github.com>
2024-06-24 23:58:57 +0300

    132: Separate client logic from server logic

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+
+/*
+ * ----------------------------------------------------------------------------
+ * "Souvenir Recommender (SORE)" Project
+ * Copyright © 2024 Moscalu Stefan and Bejenaru Matei Ioan. All rights reserved.
+ * ----------------------------------------------------------------------------
+ */
+//  pentru a face header-ul fixat dupa scroll
+window.addEventListener('scroll', function() {
+  var header = document.querySelector('header');
+  if (window.scrollY > header.offsetHeight) {
+    header.classList.add('fixed-header');
+  } else {
+    header.classList.remove('fixed-header');
+  }
+});
+
+// pentru efectul de intunecare al imaginilor si afisarea mesajului la hover
+var imageItems = document.querySelectorAll('.image-item');
+imageItems.forEach(function(item) {
+  item.addEventListener('mouseover', function() {
+    var image = item.querySelector('img');
+    image.classList.add('darken');
+  });
+  item.addEventListener('mouseout', function() {
+    var image = item.querySelector('img');
+    image.classList.remove('darken');
+  });
+});
